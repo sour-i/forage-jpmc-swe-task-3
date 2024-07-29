f5a7a1086cce994e0c209d8fab17c5a8de15c246
Souradeep <souradeep1000x@gmail.com>
2024-07-30 01:03:59 +0530

    stream ratio graph

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+  timestamp: Date 
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const ABC = serverResponds[0]
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2
+    const ratio = (priceDEF != 0) ? priceABC/priceDEF : 9
+    const upperBound = 1.04
+    const lowerBound = 0.965
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
       };
-    })
+   
   }
 }
